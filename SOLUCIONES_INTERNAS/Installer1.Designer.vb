68217d41f7257ef783424641dee5a23d55041d51
valeriasheredia <valeheredia97@gmail.com>
2020-09-17 09:00:35 -0300

    Add files via upload

---
+++ SOLUCIONES_INTERNAS/Installer1.Designer.vb
@@ -0,0 +1,67 @@
+﻿Partial Class Installer1
+    Inherits System.ComponentModel.Component
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Public Sub New(ByVal container As System.ComponentModel.IContainer)
+        MyClass.New()
+
+        'Requerido para la compatibilidad con el Diseñador de composiciones de clases Windows.Forms
+        If (container IsNot Nothing) Then
+            container.Add(Me)
+        End If
+
+    End Sub
+
+    '<System.Diagnostics.DebuggerNonUserCode()> _
+    'Public Sub New()
+    '    MyBase.New()
+
+    '    'El Diseñador de componentes requiere esta llamada.
+    '    InitializeComponent()
+
+    'End Sub
+
+    'Component reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de componentes
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de componentes requiere el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de componentes.
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(0, 0)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+
+    End Sub
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ SOLUCIONES_INTERNAS/Installer1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.ComponentModel
+Imports System.Configuration.Install
+Imports System.Configuration
+
+Public Class Installer1
+    Public Sub New()
+        MyBase.New()
+
+        'El Diseñador de componentes requiere esta llamada.
+        InitializeComponent()
+
+        'Agregue el código de inicialización después de llamar a InitializeComponent
+        Dim config As Configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
+        config.AppSettings.Settings.Remove("Title")
+        config.AppSettings.Settings.Add("Title", TextBox1.Text)
+        config.AppSettings.Settings.Remove("Number")
+        config.AppSettings.Settings.Add("Number", NumericUpDown1.Value.ToString())
+        config.Save(ConfigurationSaveMode.Modified)
+    End Sub
+End Class
