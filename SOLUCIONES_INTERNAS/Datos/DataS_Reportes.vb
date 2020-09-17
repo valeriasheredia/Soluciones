381b094912686a3dedc29ec46312821adf6c2afb
valeriasheredia <valeheredia97@gmail.com>
2020-09-17 09:03:03 -0300

    Add files via upload

---
+++ SOLUCIONES_INTERNAS/Datos/DataS_Reportes.vb
@@ -0,0 +1,21 @@
+﻿Partial Public Class DataS_Reportes
+
+
+    Partial Class DT_OrdenDataTable
+
+        Private Sub DT_OrdenDataTable_DT_OrdenRowChanging(sender As System.Object, e As DT_OrdenRowChangeEvent) Handles Me.DT_OrdenRowChanging
+
+        End Sub
+
+        'Private Sub DT_OrdenDataTable_ColumnChanging(sender As System.Object, e As System.Data.DataColumnChangeEventArgs) Handles Me.ColumnChanging
+        '    If (e.Column.ColumnName = Me.DataColumn1Column.ColumnName) Then
+        '        'Agregar código de usuario aquí
+        '    End If
+
+        'End Sub
+
+    End Class
+
+
+End Class
+
